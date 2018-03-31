16e183848baae1496fd865b364abdc656f72c432
Cristian Cancela Lema <cristian.cancela.lema@udc.es>
2018-03-31 13:49:57 +0200

    Adaptado o proxecto para a versión 0.19.0 do Composer

--- peixe/lib/CrearPeixe.js
+++ peixe/lib/CrearPeixe.js
@@ -3,7 +3,7 @@
  * @param {org.peixeencadeado.peixe.CrearPeixe} crearPeixe
  * @transaction
  */
- function crearPeixe(crearPeixe){
+ async function crearPeixe(crearPeixe){
     var NS_ORG = 'org.peixeencadeado.organizacions'
     var NS_PEIXE = 'org.peixeencadeado.peixe';
     var factory = getFactory();
@@ -11,28 +11,23 @@
     var coordenadas = factory.newConcept(NS_PEIXE,'Coordenadas');
     var peixe = factory.newResource(NS_PEIXE, 'Peixe', peixeId);
 
-    return getAssetRegistry(NS_PEIXE + '.Peixe')
-    .then(function(rexistroPeixe) {
-        coordenadas.lonxitude = crearPeixe.lonxitude;
-        coordenadas.latitude = crearPeixe.latitude;
-        peixe.coordenadas = coordenadas;
-        peixe.variedade = crearPeixe.variedade;
-        peixe.dataCaptura = new Date();
-        peixe.pesqueira = factory.newRelationship(NS_ORG, 'Pesqueira', 'pesqueira1');
-        peixe.compras = [];
-        peixe.estado = 'CAPTURADO';
-        peixe.peso = crearPeixe.peso;
-        return rexistroPeixe.add(peixe);
-    }).then(function(){
-        var evento = factory.newEvent(NS_PEIXE, 'PeixeCreado');
-        evento.peixeId = peixe.peixeId;
-        console.log(getCurrentParticipant().getFullyQualifiedIdentifier());
-        console.log(getCurrentParticipant().getFullyQualifiedIdentifier().split('#'));
-        emit(evento);
-    })
-    .catch(function(error){
-        throw new Error(error);
-    });
+    var rexistroPeixe = await getAssetRegistry(NS_PEIXE + '.Peixe')
+    coordenadas.lonxitude = crearPeixe.lonxitude;
+    coordenadas.latitude = crearPeixe.latitude;
+    peixe.coordenadas = coordenadas;
+    peixe.variedade = crearPeixe.variedade;
+    peixe.dataCaptura = new Date();
+    peixe.pesqueira = factory.newRelationship(NS_ORG, 'Pesqueira', 'pesqueira1');
+    peixe.compras = [];
+    peixe.estado = 'CAPTURADO';
+    peixe.peso = crearPeixe.peso;
+    await rexistroPeixe.add(peixe)
+    var evento = factory.newEvent(NS_PEIXE, 'PeixeCreado');
+    evento.peixeId = peixe.peixeId;
+    //console.log(getCurrentParticipant());
+    console.log('-------------------------------------------------------------------------------------------------------');
+    //console.log(getCurrentIdentity());
+    emit(evento);
 }
 
 function xerarPeixeId(){
--- peixe/test/test.js
+++ peixe/test/test.js
@@ -2,26 +2,24 @@
 
 const AdminConnection = require('composer-admin').AdminConnection;
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
-const BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefinition;
-const IdCard = require('composer-common').IdCard;
-const MemoryCardStore = require('composer-common').MemoryCardStore;
+const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
 const path = require('path');
 const chai = require('chai');
 chai.should();
 chai.use(require('chai-as-promised'));
 
-describe('Sample', () => {
+const NS_PAR = 'org.peixeencadeado.participantes';
+const NS_PEIXE = 'org.peixeencadeado.peixe';
+const NS_ORG = 'org.peixeencadeado.organizacions';
 
-    const NS_PAR = 'org.peixeencadeado.participantes';
-    const NS_PEIXE = 'org.peixeencadeado.peixe';
-    const NS_ORG = 'org.peixeencadeado.organizacions';
+describe('Sample', () => {
     // In-memory card store for testing so cards are not persisted to the file system
-    const cardStore = new MemoryCardStore();
+    const cardStore = require('composer-common').NetworkCardStoreManager.getCardStore( { type: 'composer-wallet-inmemory' } );
 
     // Embedded connection used for local testing
     const connectionProfile = {
         name: 'embedded',
-        type: 'embedded'
+        'x-type': 'embedded'
     };
 
     // Name of the business network card containing the administrative identity for the business network
@@ -45,12 +43,9 @@ describe('Sample', () => {
 
     let businessNetworkName;
 
-    before(() => {
-        // Embedded connection does not need real credentials
-        const credentials = {
-            certificate: 'FAKE CERTIFICATE',
-            privateKey: 'FAKE PRIVATE KEY'
-        };
+    before(async () => {
+        // Generate certificates for use with the embedded connection
+        const credentials = CertificateUtil.generate({ commonName: 'admin' });
 
         // Identity used with the admin connection to deploy business networks
         const deployerMetadata = {
@@ -64,18 +59,16 @@ describe('Sample', () => {
 
         adminConnection = new AdminConnection({ cardStore: cardStore });
 
-        return adminConnection.importCard(deployerCardName, deployerCard).then(() => {
-            return adminConnection.connect(deployerCardName);
-        });
+        await adminConnection.importCard(deployerCardName, deployerCard);
+        await adminConnection.connect(deployerCardName);
     });
 
     /**
      *
      * @param {String} cardName The card name to use for this identity
      * @param {Object} identity The identity details
-     * @returns {Promise} resolved when the card is imported
      */
-    function importCardForIdentity(cardName, identity) {
+    async function importCardForIdentity(cardName, identity) {
         const metadata = {
             userName: identity.userID,
             version: 1,
@@ -83,162 +76,141 @@ describe('Sample', () => {
             businessNetwork: businessNetworkName
         };
         const card = new IdCard(metadata, connectionProfile);
-        return adminConnection.importCard(cardName, card);
+        await adminConnection.importCard(cardName, card);
     }
 
     // This is called before each test is executed.
-    beforeEach(() => {
-        let businessNetworkDefinition;
-
+    beforeEach(async () => {
         // Generate a business network definition from the project directory.
-        return BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'))
-            .then(definition => {
-                businessNetworkDefinition = definition;
-                businessNetworkName = definition.getName();
-                return adminConnection.install(businessNetworkName);
-            })
-            .then(() => {
-                const startOptions = {
-                    networkAdmins: [
-                        {
-                            userName: 'admin',
-                            enrollmentSecret: 'adminpw'
-                        }
-                    ]
-                };
-                return adminConnection.start(businessNetworkDefinition, startOptions);
-            }).then(adminCards => {
-                return adminConnection.importCard(adminCardName, adminCards.get('admin'));
-            })
-            .then(() => {
-                // Create and establish a business network connection
-                businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
-                events = [];
-                businessNetworkConnection.on('event', event => {
-                    events.push(event);
-                });
-                return businessNetworkConnection.connect(adminCardName);
-            })
-            .then(() => {
-                // Get the factory for the business network.
-                factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+        let businessNetworkDefinition = await BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
+        businessNetworkName = businessNetworkDefinition.getName();
+        await adminConnection.install(businessNetworkDefinition);
+        const startOptions = {
+            networkAdmins: [
+                {
+                    userName: 'admin',
+                    enrollmentSecret: 'adminpw'
+                }
+            ]
+        };
+        const adminCards = await adminConnection.start(businessNetworkName, businessNetworkDefinition.getVersion(), startOptions);
+        await adminConnection.importCard(adminCardName, adminCards.get('admin'));
 
-                return businessNetworkConnection.getParticipantRegistry(NS_PAR + '.Participante');
-            })
-            
-            .then(participantRegistry => {
-                // Create the participants.
-                const alice = factory.newResource(NS_PAR, 'Participante', 'alice@email.com');
-                alice.nome = 'Alice';
+        // Create and establish a business network connection
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+        events = [];
+        businessNetworkConnection.on('event', event => {
+            events.push(event);
+        });
+        await businessNetworkConnection.connect(adminCardName);
 
-                const bob = factory.newResource(NS_PAR, 'Participante', 'bob@email.com');
-                bob.nome = 'Bob';
+        // Get the factory for the business network.
+        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
 
-                participantRegistry.addAll([alice, bob]);
-            })
-            .then(() => {
-                return businessNetworkConnection.getAssetRegistry(NS_ORG + '.Pesqueira');
-            })
-            .then(function(rexistroPesqueira) {
-                var pesqueira = factory.newResource(NS_ORG, 'Pesqueira','pesqueira1');
-                pesqueira.email = pesqueira.orgId;
-                pesqueira.administrador = factory.newRelationship(NS_PAR, 'Participante', 'participante1');
-                return rexistroPesqueira.add(pesqueira);
-            })
-            .then(function() {
-                return businessNetworkConnection.getAssetRegistry(NS_ORG + '.Empresa');
-            })
-            .then(function(rexistroEmpresa) {        
-                var empresa = factory.newResource(NS_ORG, 'Empresa','empresa1');
-                empresa.email = empresa.orgId;
-                empresa.administrador = factory.newRelationship(NS_PAR, 'Participante', 'participante2');
-                return rexistroEmpresa.add(empresa);
-            })
-            .then(function() {
-            return businessNetworkConnection.getAssetRegistry(NS_PEIXE + '.Peixe');
-            })
-            .then(function(rexistroPeixe) {
-                var peixes = [
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe1'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe2'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe3'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe4'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe5'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe6'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe7'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe8'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe9'),
-                    factory.newResource(NS_PEIXE, 'Peixe', 'peixe10')
-                ];
-                peixes.forEach(function(peixe, index) {
-                    var coordenadas = factory.newConcept(NS_PEIXE,'Coordenadas');
-                    coordenadas.lonxitude = 3.14;
-                    coordenadas.latitude = -74.23;
-                    peixe.coordenadas = coordenadas;
-                    peixe.variedade = 'xurelo';
-                    peixe.dataCaptura = new Date();
-                    peixe.pesqueira = factory.newRelationship(NS_ORG, 'Pesqueira', 'pesqueira1');
-                    peixe.compras = [];
-                    peixe.estado = 'CAPTURADO';
-                    peixe.peso = 1.1;
-                });
-                return rexistroPeixe.addAll(peixes);
-            })
-            .then(() => {
-                // Issue the identities.
-                return businessNetworkConnection.issueIdentity(NS_PAR + '.Participante#alice@email.com', 'alice1');
-            })
-            .then(identity => {
-                return importCardForIdentity(aliceCardName, identity);
-            }).then(() => {
-                return businessNetworkConnection.issueIdentity(NS_PAR + '.Participante#bob@email.com', 'bob1');
-            })
-            .then((identity) => {
-                return importCardForIdentity(bobCardName, identity);
+        const participantRegistry = await businessNetworkConnection.getParticipantRegistry(NS_PAR + '.Participante');
+        // Create the participants.
+        const alice = factory.newResource(NS_PAR, 'Participante', 'alice@email.com');
+        alice.nome = 'Alice';
+
+        const bob = factory.newResource(NS_PAR, 'Participante', 'bob@email.com');
+        bob.nome = 'Bob';
+
+        participantRegistry.addAll([alice, bob]);
+
+        const rexistroPesqueira = await businessNetworkConnection.getAssetRegistry(NS_ORG + '.Pesqueira')
+        var pesqueira = factory.newResource(NS_ORG, 'Pesqueira','pesqueira1');
+        pesqueira.email = pesqueira.orgId;
+        pesqueira.administrador = factory.newRelationship(NS_PAR, 'Participante', 'participante1');
+        await rexistroPesqueira.add(pesqueira)
+        const rexistroEmpresa = await businessNetworkConnection.getAssetRegistry(NS_ORG + '.Empresa');      
+        var empresa = factory.newResource(NS_ORG, 'Empresa','empresa1');
+        empresa.email = empresa.orgId;
+        empresa.administrador = factory.newRelationship(NS_PAR, 'Participante', 'participante2');
+        return rexistroEmpresa.add(empresa)
+        .then(function() {
+        return businessNetworkConnection.getAssetRegistry(NS_PEIXE + '.Peixe');
+        })
+        .then(function(rexistroPeixe) {
+            var peixes = [
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe1'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe2'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe3'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe4'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe5'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe6'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe7'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe8'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe9'),
+                factory.newResource(NS_PEIXE, 'Peixe', 'peixe10')
+            ];
+            peixes.forEach(function(peixe, index) {
+                var coordenadas = factory.newConcept(NS_PEIXE,'Coordenadas');
+                coordenadas.lonxitude = 3.14;
+                coordenadas.latitude = -74.23;
+                peixe.coordenadas = coordenadas;
+                peixe.variedade = 'xurelo';
+                peixe.dataCaptura = new Date();
+                peixe.pesqueira = factory.newRelationship(NS_ORG, 'Pesqueira', 'pesqueira1');
+                peixe.compras = [];
+                peixe.estado = 'CAPTURADO';
+                peixe.peso = 1.1;
             });
+            return rexistroPeixe.addAll(peixes);
+        })
+        .then(() => {
+            // Issue the identities.
+            return businessNetworkConnection.issueIdentity(NS_PAR + '.Participante#alice@email.com', 'alice1');
+        })
+        .then(identity => {
+            return importCardForIdentity(aliceCardName, identity);
+        }).then(() => {
+            return businessNetworkConnection.issueIdentity(NS_PAR + '.Participante#bob@email.com', 'bob1');
+        })
+        .then((identity) => {
+            return importCardForIdentity(bobCardName, identity);
+        });
     });
 
     /**
      * Reconnect using a different identity.
      * @param {String} cardName The name of the card for the identity to use
-     * @return {Promise} A promise that will be resolved when complete.
      */
-    function useIdentity(cardName) {
-        return businessNetworkConnection.disconnect()
-            .then(() => {
-                businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
-                events = [];
-                businessNetworkConnection.on('event', (event) => {
-                    events.push(event);
-                });
-                return businessNetworkConnection.connect(cardName);
-            })
-            .then(() => {
-                factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-            });
+    async function useIdentity(cardName) {
+        await businessNetworkConnection.disconnect();
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+        events = [];
+        businessNetworkConnection.on('event', (event) => {
+            events.push(event);
+        });
+        await businessNetworkConnection.connect(cardName);
+        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
     }
 
-    it('Alice can read all of the assets', () => {
+    it('Execución da transaccion CrearPeixe', async () => {
 
         // Use the identity for Alice.
-        return useIdentity(aliceCardName)
-            .then(() => {
+        await useIdentity(aliceCardName)
+        let transaction = factory.newTransaction("org.peixeencadeado.peixe", "CrearPeixe");
+        transaction.setPropertyValue('variedade', 'SARDIÑA');
+        transaction.setPropertyValue('peso', 1.28);
+        transaction.setPropertyValue('latitude', -1.45);
+        transaction.setPropertyValue('lonxitude', 3.14);
 
-                // Get the assets.
-                return businessNetworkConnection.getAssetRegistry(NS_PEIXE + '.Peixe')
-                    .then((assetRegistry) => {
-                        return assetRegistry.getAll();
-
-                    });
-
-            })
-            .then((assets) => {
-
-                // Validate the assets.
-                assets.should.have.lengthOf(10);
-            });
-
+        await businessNetworkConnection.submitTransaction(transaction);
     });
 
+    it('Comporobación de lectura no blockchain por parte de alice', () => {
 
+        return useIdentity(aliceCardName)
+        .then(() => {
+            return businessNetworkConnection.getAssetRegistry(NS_PEIXE + '.Peixe')
+            .then((assetRegistry) => {
+                return assetRegistry.getAll();
+            });
+        })
+        .then((assets) => {
+            // Validate the assets.
+            assets.should.have.lengthOf(10);
+        });
+    });
 });
